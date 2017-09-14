d72a1001e831fb1a96a0a65fa11ae3b8da67f719
Herri Fransisca <herrifransisca@outlook.com>
2017-09-14 19:23:06 +0700

    Radio Buttons.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MdCheckboxModule } from '@angular/material';
+import { MdCheckboxModule, MdRadioModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MdCheckboxModule
+    MdCheckboxModule,
+    MdRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
